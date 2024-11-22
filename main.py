65b8d8a095ffa7208344ca0c0304cef22612ec8d
Bilal Ahmed <iambilalahmed1999@gmail.com>
2024-11-22 23:48:52 +0500

    ls

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 def main():
     r = requests.get("https://google.com")
-    print(f"statu {r.status_code}")
+    print(f"status {r.status_code}")
 
 if __name__ == "__main__":
     main()
