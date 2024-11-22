7eb9bfc2dc95f4ea963cff4e286942382bd405d9
Bilal Ahmed <iambilalahmed1999@gmail.com>
2024-11-22 21:42:56 +0500

    env place change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 def main():
     r = requests.get("https://google.com")
-    print(f"statusss {r.status_code}")
+    print(f"statu {r.status_code}")
 
 if __name__ == "__main__":
     main()
